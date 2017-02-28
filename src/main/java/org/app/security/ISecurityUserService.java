32af5635fc45667d2aa13793d756ba2c7e872155
unknown <manojsingh3889@yahoo.com>
2017-02-28 22:20:49 +0530

    ...

--- src/main/java/org/app/security/ISecurityUserService.java
+++
@@ -1,7 +0,0 @@
-package org.app.security;
-
-public interface ISecurityUserService {
-
-    String validatePasswordResetToken(long id, String token);
-
-}
--- src/main/java/org/app/spring/ServiceConfig.java
+++
@@ -1,9 +0,0 @@
-package org.app.spring;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({ "org.baeldung.service" })
-public class ServiceConfig {
-}
