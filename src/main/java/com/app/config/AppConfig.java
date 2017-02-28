256c46d772256102a5ddc20e0f5c393d526a8b43
unknown <manojsingh3889@yahoo.com>
2017-02-28 22:42:50 +0530

    //

--- src/main/java/com/app/config/AppConfig.java
+++ src/main/java/com/app/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.app.spring")
+@ComponentScan(basePackages = "com.app")
 public class AppConfig {
 
 }
