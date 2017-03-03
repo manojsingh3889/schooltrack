0d95f489a4f61670a02e390643dcdf158f541b48
unknown <manojsingh3889@yahoo.com>
2017-03-03 22:23:09 +0530

    corrections

--- src/main/java/com/app/controller/UserMgmtController.java
+++ src/main/java/com/app/controller/UserMgmtController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.app.model.LoginBean;
 import com.app.model.RegisterBean;
+import com.app.um.LoginManager;
 import com.app.um.LoginService;
 
 
@@ -18,15 +19,15 @@ import com.app.um.LoginService;
 public class UserMgmtController {
 
 	@Autowired
-	private LoginService loginservice;
+	private LoginManager loginManager;
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/login")
 	public ResponseEntity login(@RequestBody LoginBean bean) {
-		return new ResponseEntity(loginservice.authenticateUser(bean.getUsername(), bean.getPassword()), HttpStatus.OK);
+		return new ResponseEntity(loginManager.authenticateUser(bean.getEmail(), bean.getPassword()),HttpStatus.OK);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/register")
-	public ResponseEntity login(@RequestBody RegisterBean bean){
-		return new ResponseEntity(loginservice.registerUser(bean), HttpStatus.OK);
+	public ResponseEntity register(@RequestBody RegisterBean bean){
+		return new ResponseEntity("under construction", HttpStatus.OK);
 	}
 }
--- src/main/java/com/app/model/LoginBean.java
+++ src/main/java/com/app/model/LoginBean.java
@@ -1,17 +1,22 @@
 package com.app.model;
 
 public class LoginBean {
-	private String username;
+	private String email;
 	private String password;
-	public LoginBean(String username, String password) {
-		this.username = username;
+	public LoginBean(String email, String password) {
+		super();
+		this.email = email;
 		this.password = password;
 	}
-	public String getUsername() {
-		return username;
+	public LoginBean() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
-	public void setUsername(String username) {
-		this.username = username;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	public String getPassword() {
 		return password;
@@ -19,10 +24,6 @@ public class LoginBean {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public LoginBean() {
-	}
-	
-	
 	
 	
 	
